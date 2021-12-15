8f2ff4059a54fdbef3ef70a3e9978d47559602f3
Alesis Vasquez <alesis.vasquez@mercadolibre.com>
2021-12-15 17:41:04 -0300

    Archivos agregados por el Participante B

---
+++ Vehiculo.java
@@ -0,0 +1,7 @@
+package misClases
+
+class Vehiculo {
+    int pasajeros;  //números de pasajeros
+    int capacidad;  //capacidad del combustible en galones
+    int mpg;        //combustible consumido en millas por galon
+}
